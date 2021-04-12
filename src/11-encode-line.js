b1ba4e96458d5cc5e5b5e87bcf96d11dcf393dca
Lina Gorihvostka <lina.editor@gmail.com>
2021-04-13 00:12:00 +0300

    feat: solve number 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,29 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(string) {
+  const array = string.split('');
+  let counter = 1;
+  let letter = '';
+  const encoded = [];
+  for (let i = 0; i < array.length; i++) {
+    letter = array[i];
+    if (array[i - 1] === array[i]) {
+      counter++;
+    } else if (i > 0) {
+      if (array[i - 1] !== array[i]) {
+        encoded.push(counter);
+        encoded.push(array[i - 1]);
+        counter = 1;
+      }
+    }
+  }
+  encoded.push(counter);
+  encoded.push(letter);
+  const re = /1/g;
+  let result = encoded.join('');
+  result = result.replace(re, '');
+  return result;
 }
 
 module.exports = encodeLine;

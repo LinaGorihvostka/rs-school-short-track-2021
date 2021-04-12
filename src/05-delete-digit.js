0a025b3edcbce85a860310ffca71db04fc0cbe0b
Lina Gorihvostka <lina.editor@gmail.com>
2021-04-13 01:58:25 +0300

    feat: solve number 5 task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,25 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(number) {
+  const array = number.toString().split('');
+  let min = '';
+  if (array.length === 2) {
+    min = array[0] < array[1] ? array[0] : array[1];
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < array[i + 1]) {
+        min = array[i];
+      }
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === min) {
+      array.splice(i, 1);
+    }
+  }
+  const result = array.join('');
+  return +result;
 }
 
 module.exports = deleteDigit;
